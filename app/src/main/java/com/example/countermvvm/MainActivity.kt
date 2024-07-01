b89532fe0b9670f11adf9a42eb3e80b82c20f272
Moath Alkhelifi <malkhelifi@rize.sa>
2024-07-01 19:54:10 +0300

    Project Initialized & And Counter Working

---
+++ app/src/main/java/com/example/countermvvm/MainActivity.kt
@@ -0,0 +1,77 @@
+package com.example.countermvvm
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.countermvvm.ui.theme.CounterMVVMTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CounterMVVMTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    TheCounterApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TheCounterApp(){
+    val counter = remember{ mutableIntStateOf(0) }
+
+    fun increment(){
+        counter.intValue++
+    }
+
+    fun decrement(){
+        counter.intValue--
+    }
+
+    Column(modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+        Text(counter.intValue.toString(), fontSize = 30.sp)
+        Row(modifier = Modifier.padding(top = 8.dp).fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            ){
+            Button(onClick = { decrement() }) {
+                Text("-")
+            }
+
+            Button(onClick = { increment() }) {
+                Text("+")
+            }
+        }
+    }
+
+}
